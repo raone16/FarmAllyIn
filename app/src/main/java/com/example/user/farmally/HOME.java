3c8807caeab7533baf0e5ea818424386777f3e8a
raone16 <raveenasenthilvel@gmail.com>
2018-03-13 15:48:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/farmally/HOME.java
@@ -0,0 +1,13 @@
+package com.example.user.farmally;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HOME extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
